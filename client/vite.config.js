d0be7d103c77ad810c6911a54ecc6fdae6fa056d
madhavbanjade <madhavbanjade005@gmail.com>
2025-06-15 09:56:21 +0545

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://realestate-system.onrender.com/api",
-         changeOrigin: true,
+        target: "http://localhost:3000",
+        changeOrigin: true,
       },
     },
   },
